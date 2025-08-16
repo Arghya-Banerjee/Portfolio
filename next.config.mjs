6e62912f1606623b8f6d994c18ed842ad86469d0
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-08-16 19:21:40 +0530

    Small Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  images: {
+    remotePatterns: [
+      // GitHub raw sprites or other CDNs you use
+      { protocol: "https", hostname: "raw.githubusercontent.com" },
+      // If you load Google Drive thumbnails:
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      // If you embed direct Drive file links (rarely works with Next/Image):
+      { protocol: "https", hostname: "drive.google.com" }
+    ]
+  }
 };
-export default nextConfig;
+module.exports = nextConfig;
